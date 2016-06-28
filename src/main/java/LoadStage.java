dac8b799ceb69fe739779a9ca9a02b5510595b42
Lionel <lionel.schockenhoff@hhu.de>
2016-06-28 13:04:23 +0200

    testcopycommit

--- src/main/java/LoadStage.java
+++ src/main/java/LoadStage.java
@@ -1,14 +1,16 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 /**
  * Created by Lionel on 28.06.2016.
  */
 public class LoadStage {
-
-
+    
     public LoadStage(){
         try {
             Parent root = FXMLLoader.load(getClass().getResource("load.fxml"));
@@ -20,6 +22,9 @@ public class LoadStage {
             ex.printStackTrace();
         }
 
-
+        ListView<String> list = new ListView<String>();
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "Single", "Double", "Suite", "Family App");
+        list.setItems(items);
     }
 }
