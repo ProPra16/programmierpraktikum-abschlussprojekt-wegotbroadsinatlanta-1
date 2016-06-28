774a7ffc5e795ac14105b257127306d31b4fd267
thosch777 <thomas.schmidt92@gmx.de>
2016-06-28 13:09:42 +0200

    klasse AufgabeKlasse erstellt

---
+++ src/main/java/FileIO/Aufgabe.java
@@ -0,0 +1,9 @@
+package FileIO;
+
+/**
+ * Created by thosch777 on 28.06.16.
+ */
+public class Aufgabe {
+    String name = "";
+    String description = "";
+}
---
+++ src/main/java/FileIO/AufgabeKlasse.java
@@ -0,0 +1,9 @@
+package FileIO;
+
+/**
+ * Created by thosch777 on 28.06.16.
+ */
+public class AufgabeKlasse {
+    String name = "";
+    String vorlage = "";
+}
