2b1713f901744509fb9cdd4c6321dd1aa265e820
Felix Kerlin <felix.kerlin@web.de>
2016-06-28 13:15:33 +0200

    FileIO read/write dummy methods

--- src/main/java/FileIO/FileIO.java
+++ src/main/java/FileIO/FileIO.java
@@ -1,6 +1,14 @@
 package FileIO;
 
+import java.util.ArrayList;
+
 public class FileIO {
+    public static void writeKatalog(ArrayList<Aufgabe> katalog) {
 
+    }
 
+    public static ArrayList<Aufgabe> readKatalog(){
+        ArrayList<Aufgabe> katalog = new ArrayList<Aufgabe>();
+        return katalog;
+    }
 }
