d19b3bb2e8a282ae40a81cf153874d35ebb52110
thosch777 <thomas.schmidt92@gmx.de>
2016-06-28 13:18:51 +0200

    klasse AufgabeTests erstellt klasse Aufgabe erweitert

--- src/main/java/FileIO/Aufgabe.java
+++ src/main/java/FileIO/Aufgabe.java
@@ -1,9 +1,13 @@
 package FileIO;
 
+import java.util.ArrayList;
+
 /**
  * Created by thosch777 on 28.06.16.
  */
 public class Aufgabe {
     String name = "";
     String description = "";
+    ArrayList<AufgabeKlasse> aufgabeklassen = new ArrayList<AufgabeKlasse>();
+    ArrayList<AufgabeTest> aufgabetests = new ArrayList<AufgabeTest>();
 }
