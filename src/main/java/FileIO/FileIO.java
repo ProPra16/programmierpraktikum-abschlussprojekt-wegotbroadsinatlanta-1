265f095846fc25ca35370c4245c0eaec11c2f1fe
Felix Kerlin <felix.kerlin@web.de>
2016-07-01 11:41:05 +0200

    added reader / writer for fileIO

--- src/main/java/FileIO/FileIO.java
+++ src/main/java/FileIO/FileIO.java
@@ -1,15 +1,33 @@
 package FileIO;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class FileIO {
 
-    public static void writeKatalog(ArrayList<Aufgabe> katalog) {
+    private static Writer writer;
 
+    private static Reader reader;
+
+    private static String filename = "catalog.txt";
+
+    public static void writeKatalog(ArrayList<Aufgabe> katalog) {
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8"));
+        } catch (Exception e) {
+            System.out.println("FILE NOT FOUND");
+        }
     }
 
     public static ArrayList<Aufgabe> readKatalog(){
         ArrayList<Aufgabe> katalog = new ArrayList<Aufgabe>();
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "utf-8"));
+        } catch (Exception e) {
+            System.out.println("FILE NOT FOUND");
+        }
+
         return katalog;
     }
 }
