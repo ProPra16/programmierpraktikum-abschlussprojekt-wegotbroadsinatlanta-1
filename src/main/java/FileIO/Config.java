db975eb0d3124b8de8103f57473a8fc55ee8816f
Felix Kerlin <felix.kerlin@web.de>
2016-07-01 11:50:54 +0200

    should compile now

--- src/main/java/FileIO/Config.java
+++ src/main/java/FileIO/Config.java
@@ -4,11 +4,11 @@ package FileIO;
  * Created by thosch777 on 28.06.16.
  */
 public class Config {
-    Babystep babystep;
+    //Babystep babystep;
     boolean timetracking;
 
-    public Config(Babystep babystep, boolean timetracking) {
-        this.babystep = babystep;
+    public Config(/*Babystep babystep,*/ boolean timetracking) {
+        //this.babystep = babystep;
         this.timetracking = timetracking;
     }
 
