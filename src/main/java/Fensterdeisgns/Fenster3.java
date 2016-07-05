023ec07941eb016218d9416e8b737b93acf02bbd
AndranikSar <andosarkis94@hotmail.com>
2016-07-05 14:55:46 +0200

    Bar chart wird erstellt für die Auswertung +++++++AndranikSar

--- src/main/java/Fensterdeisgns/Fenster3.java
+++ src/main/java/Fensterdeisgns/Fenster3.java
@@ -1,7 +1,51 @@
 package Fensterdeisgns;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 /**
  * Created by Ando Mando on 05.07.2016.
  */
-public class Fenster3 {
+public class Fenster3 extends Application {
+    Stage window;
+    Button abbrechen, neustart;
+
+    private void fensterkonstrukt(Stage window){
+        abbrechen = new Button("Abbrechen");
+        neustart = new Button("Neustart");
+
+        neustart.setOnAction(e -> {
+            //Zurück zu Fenster 1
+        });
+
+        abbrechen.setOnAction(e -> {
+           window.close();
+        });
+
+        HBox layout = new HBox();
+        layout.getChildren().addAll(neustart, abbrechen);
+
+        BorderPane borderpane = new BorderPane();
+        borderpane.setBottom(layout);
+
+        Scene scene = new Scene(borderpane, 600, 400);
+        window.setScene(scene);
+
+        window.show();
+    }
+
+    /////////////////////////////////////////////////////TEST-AUSFÜHRUNG
+    public static void main(String[]args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primarystage){
+        window = primarystage;
+        fensterkonstrukt(window);
+    }
 }
--- src/main/java/TDDTController.java
+++ src/main/java/TDDTController.java
@@ -1,6 +1,12 @@
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,4 +37,49 @@ public class TDDTController implements Initializable{
     }
 
 
+    /**
+     * Created by Ando Mando on 05.07.2016.
+     */
+    public static class Fenster3 extends Application {
+        Button neustart, abbrechen;
+        Stage window;
+
+        public void fensterkonstrukt(Stage window){
+
+            neustart = new Button("Neustart");
+            abbrechen = new Button("Abbrechen");
+
+            neustart.setOnAction(e -> {
+                ;
+            });
+
+            abbrechen.setOnAction(e -> {
+                window.close();
+                    });
+
+            VBox layout = new VBox(10);
+            layout.getChildren().addAll(neustart, abbrechen);
+
+            Scene scene = new Scene(layout, 700, 500);
+            window.setScene(scene);
+
+            window.show();
+
+        }
+
+
+
+
+        ////////////////////////////////////////TEST-AUSFÜHRUNG
+        public static void main(String[] args){
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage primarystage){
+            window = primarystage;
+            fensterkonstrukt(window);
+        }
+
+    }
 }
