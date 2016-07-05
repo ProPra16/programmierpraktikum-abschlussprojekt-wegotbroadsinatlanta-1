b2cf85fae7816ff9a5b462371091ef63b0a72f7f
Lionel <lionel.schockenhoff@hhu.de>
2016-07-05 12:19:03 +0200

    Added Function to Launch Main with Task settings // ++++Ando+++

--- src/main/java/TDDT.java
+++ src/main/java/TDDT.java
@@ -1,4 +1,9 @@
 import FileIO.Project;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
 
 /**
  * Created by Lionel on 28.06.2016.
@@ -13,5 +18,13 @@ public class TDDT {
     public void setTask(Project p){
         this.Task = p;
         System.out.println(p.code + p.test); //Debug Project übergabe
+        try{
+            FXMLLoader mainloader = new FXMLLoader(getClass().getResource("sample.fxml"));
+            Parent mainparent = mainloader.load();
+            Main.Bp.setCenter(mainparent);
+            TDDTController tcontroller = mainloader.getController();
+            tcontroller.setLeftTextArea(p.code);
+            tcontroller.setRightTextArea(p.test);
+        }catch (IOException ex){ex.printStackTrace();}
     }
 }
