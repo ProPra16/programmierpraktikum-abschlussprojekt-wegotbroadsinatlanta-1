247d49d40afd8f96415a327400113c3155059920
Felix Kerlin <felix.kerlin@web.de>
2016-07-14 13:22:28 +0200

    refactored project added some packages

--- src/main/java/DEBUG.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Lionel on 01.07.2016.
- */
-public class DEBUG {
-    public static void out(String s){
-        System.err.println("DEBUG::  " + s);
-    }
-}
--- src/main/java/FileIO/Config.java
+++
@@ -1,9 +0,0 @@
-package FileIO;
-
-/**
- * Created by thosch777 on 28.06.16.
- */
-public class Config {
-    BabystepConfig babystep = new BabystepConfig();
-    String timetracking;
-}
