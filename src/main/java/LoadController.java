ab21f808c9bfd57ac5f1eb5408c0d8abad96cf42
Lionel <lionel.schockenhoff@hhu.de>
2016-07-14 16:22:41 +0200

    Added halfway API loadtask -- ready for further development

--- src/main/java/LoadController.java
+++ src/main/java/LoadController.java
@@ -1,4 +1,6 @@
 //import FileIO.Project;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,30 +11,29 @@ import javafx.scene.control.ListView;
  * Created by Lionel on 28.06.2016.
  */
 public class LoadController {       //Design des FXML muss überarbeitet werden  ---> Ando
-    private TDDT TDDT = new TDDT();
     @FXML
     ListView list;
-    Button but;
+    @FXML Button startTDDT;
     ObservableList<String> items = FXCollections.observableArrayList (
             "Aufgabe: 1", "Aufgabe: 2", "Aufgabe: 3", "Aufgabe: 4", "Aufgabe: 1", "Aufgabe: 2", "Aufgabe: 3", "Aufgabe: 4", "Aufgabe: 1", "Aufgabe: 2", "Aufgabe: 3", "Aufgabe: 4", "Aufgabe: 1", "Aufgabe: 2", "Aufgabe: 3", "Aufgabe: 4"); ///Die Aufgabenstellungen müssen noch eingefügt werden
-    @FXML void initialize(){
+
+    void initialize(){
         list.setItems(items);
-        /*list.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {       // Auswahl ohne Select Button
+        list.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {       // Auswahl ohne Select Button
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
                 System.out.println("Selected item: " + newValue);
             }
-        });*/
-        selectTask();
+        });
     }
 
-    public void startmain(int task) { //Ruft Main mit Parametern auf also Auswahl der Aufgabe
-        DEBUG.out("Starting TDDT with Task Number: " + task);
-        //TDDT.setTask(new Project("code ","testjunitfile"));
+    public void startTDDT(){
+        Main.self.Bp.setCenter(Main.self.root);
+        loadTask("code","test","23",true,true);
     }
 
-    @FXML void selectTask(){
-        startmain(1);
-        System.out.println(list.getSelectionModel().selectedIndexProperty().toString());     // Get Index to select a Task and start main Stage with selection
+    public void loadTask(String code, String test, String taskname, boolean babysteps, boolean Timetracking){
+        Main.tcontroller.setLeftTextArea(code);
+        Main.tcontroller.setRightTextArea(test);
     }
 }
